9685de3063cb59a32a3a03afebbc342b161f6bb0
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-24 20:45:03 -0500

    added responseModel

---
+++ Fetch-N-Store/Fetch-N-Store/Models/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fetch_N_Store.Models
+{
+    public class ResponseModel
+    {
+        [Key]
+        public int ResponseId { get; set; }
+        [Required]
+        public int StatusCode { get; set; }
+        [Required]
+        public string URL { get; set; }
+        [Required]
+        public DateTime ResponseTime { get; set; }
+    }
+}
