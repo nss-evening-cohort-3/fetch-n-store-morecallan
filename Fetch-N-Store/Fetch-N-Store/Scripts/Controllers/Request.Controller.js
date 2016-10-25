36a409ff1a0b4b757b74cf33933d53822697e1f7
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-24 20:34:27 -0500

    updated UI to better work with latter steps

--- Fetch-N-Store/Fetch-N-Store/Scripts/Controllers/Request.Controller.js
+++ Fetch-N-Store/Fetch-N-Store/Scripts/Controllers/Request.Controller.js
@@ -1,13 +1,19 @@
 ﻿app.controller("RequestCtrl", function ($scope, RequestFactory) {
 
-    $scope.response = "";
+    $scope.responses = [];
 
     $scope.collectData = () => {
-        console.log($scope.method)
-        console.log($scope.URL)
-        RequestFactory.getRequest($scope.method, $scope.URL).then(response => {
+
+        RequestFactory.getRequest($scope.method, $scope.URL).then(responseObj => {
+            let response = {}
+            response.statusCode = responseObj.status;
+            response.URL = $scope.URL;
+            response.method = $scope.method;
+            response.responseTime = responseObj.headers;
             // the response status code, URL, HTTP method used for the original request and response time should be displayed on the page.
-            $scope.response = `Status Code: ${response.status}, URL: ${$scope.URL}, HTTP METHOD: ${$scope.method}, RESPONSE TIME: ${response.headers}`
+            $scope.responses.push(response)
+
+            $scope.URL = '';
         })
     }
 
